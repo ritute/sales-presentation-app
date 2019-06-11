30e661403f5fe40c9249d83e829b4049b57406d2
ritute <rita@ritute.com>
2019-06-10 20:43:23 -0400

    Updates

---
+++ precache-manifest.e5198eebe861de15eb660f206c4ddd0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10a3e4271d35009501b4",
+    "url": "/sales-presentation-app/static/js/main.932a825b.chunk.js"
+  },
+  {
+    "revision": "2a9072a0780dbbb50379",
+    "url": "/sales-presentation-app/static/js/runtime~main.cbd28501.js"
+  },
+  {
+    "revision": "a97ddefc23744f81940a",
+    "url": "/sales-presentation-app/static/js/2.dc524ad2.chunk.js"
+  },
+  {
+    "revision": "dbce7bfe93b7c1f3254e9c91710240a5",
+    "url": "/sales-presentation-app/static/media/avatar.dbce7bfe.jpeg"
+  },
+  {
+    "revision": "39e6ac471ab9fa748e6e560243ddb621",
+    "url": "/sales-presentation-app/static/media/empty-folder.39e6ac47.png"
+  },
+  {
+    "revision": "6945e1b05405fe461d0aa4ea8cd2be14",
+    "url": "/sales-presentation-app/index.html"
+  }
+];
